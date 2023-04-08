70b98bf31f64b92ef7cce6684a5eeb47a1fda73e
gourav <dhatrireddy25@gmail.com>
2023-04-08 17:46:23 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+//import ProjectPage from './components/projectpage';
+import React from 'react';
+//import HomePage from './components/homepage';
+import { Route } from 'react-router-dom';
+
+import Allmeetupspage from './pages/AllMeetup';
+import Newmeetupspage from './pages/NewMeetups';
+import Favoritespage from './pages/Favorites';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path='/'> 
+      <Allmeetupspage/>
+      </Route>
+
+      <Route path='/new-meetup'> 
+      <Newmeetupspage/>
+      </Route>
+
+      <Route path='/favorite'> 
+      <Favoritespage/>
+      </Route>
     </div>
+
   );
 }
-
 export default App;
