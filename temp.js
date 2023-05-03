798fce3c30eb994ad6a604dd84d7677c02044cf1
gourav <dhatrireddy25@gmail.com>
2023-05-04 00:27:22 +0530

    updated project pages

---
+++ temp.js
@@ -0,0 +1,9 @@
+// if("selectedStatus" == "rejected" && "approvedDate" !== null){
+//     //?fetch req for reejcted
+// } else if("selectedStatus" == "accepted" && "approvedDate" !== null && "startDate" !== null "endDate" !== null && "startdate" < "enddate"){
+//     //?fetch request for acceptwed
+// } else {
+//     alert("dates havent filled properly")
+// }
+
+
