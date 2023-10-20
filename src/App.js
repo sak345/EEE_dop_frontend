e9d425c9d569ba3362514a43f4946e6416172e83
Harshit Bhansali <f20200416@hyderabad.bits-pilani.ac.in>
2023-10-20 16:16:58 +0530

    download func added

--- src/App.js
+++ src/App.js
@@ -19,108 +19,31 @@ import DownloadProjectsPage from './pages/Projects/downloadprojects'
 function App() {
   const [isAdmin, setIsAdmin] = useState(false)
 
-  useEffect(() => {
-    let confi = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: process.env.REACT_APP_BACKEND_URL + 'user/me',
-      headers: {
-        Authorization: localStorage.getItem('Token'),
-      },
-    }
-    // Make a GET request to the server to get the user's role
-    axios
-      .get(confi)
-      .then((response) => {
-        const { role } = response.data
+  // useEffect(() => {
+  //   let confi = {
+  //     method: 'get',
+  //     maxBodyLength: Infinity,
+  //     url: process.env.REACT_APP_BACKEND_URL + 'user/me',
+  //     headers: {
+  //       Authorization: localStorage.getItem('Token'),
+  //     },
+  //   }
+  //   // Make a GET request to the server to get the user's role
+  //   axios
+  //     .get(confi)
+  //     .then((response) => {
+  //       const { role } = response.data
 
-        // If the user is an admin, set the state to true
-        if (role === 'admin') {
-          setIsAdmin(true)
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }, [])
+  //       // If the user is an admin, set the state to true
+  //       if (role === 'admin') {
+  //         setIsAdmin(true)
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error(error)
+  //     })
+  // }, [])
 
-  if (isAdmin) {
-    return (
-      <BrowserRouter >
-        <Routes>
-          <Route path='/' element={<LogIn />} />
-          <Route
-            path='/'
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path='/project'
-            element={
-              <PrivateRoute>
-                <ProjectPage />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path='/submittedproject'
-            element={
-              <PrivateRoute>
-                <SubmittedProjectsPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path='/rejectedproject'
-            element={
-              <PrivateRoute>
-                <RejectedProjectsPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path='/enterproject'
-            element={
-              <PrivateRoute>
-                <EnterProjectsPage />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path='/ongoingproject'
-            element={
-              <PrivateRoute>
-                <OngoingProjectsPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path='/completedproject'
-            element={
-              <PrivateRoute>
-                <CompletedProjectsPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path='/downloadproject'
-            element={
-              <PrivateRoute>
-                <DownloadProjectsPage />
-              </PrivateRoute>
-            }
-          />
-
-          <Route path='/a' element={<APage />} />
-        </Routes>
-      </BrowserRouter>
-    )
-  }
 
   return (
     <BrowserRouter >
@@ -182,7 +105,7 @@ function App() {
             </PrivateRoute>
           }
         />
-        {/* <Route path="/downloadproject" element={<PrivateRoute><DownloadProjectsPage/></PrivateRoute>} /> */}
+      <Route path="/downloadproject" element={<PrivateRoute><DownloadProjectsPage/></PrivateRoute>} /> 
 
         <Route path='/a' element={<APage />} />
       </Routes>
